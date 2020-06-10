6be18618c938bbaf7c2dc22f910eff484f832ec7
dhiaeddineTounakti <dhiaeddine.git@outlook.com>
2020-06-10 02:46:59 +0200

    finished preprocessing

--- src/provider.py
+++ src/provider.py
@@ -1,5 +1,5 @@
 """Cifar10 dataset preprocessing and specifications."""
-
+#%%
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -7,9 +7,12 @@ from __future__ import print_function
 import os
 import tarfile
 import numpy as np
-from six.moves import cPickle
+import pickle
 from six.moves import urllib
 import tensorflow as tf
+import cv2
+from scipy import signal
+import matplotlib.pyplot as plt
 
 REMOTE_URL = "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz"
 LOCAL_DIR = os.path.join("data/cifar10/")
@@ -40,9 +43,10 @@ def prepare():
         tar = tarfile.open(LOCAL_DIR + ARCHIVE_NAME)
         tar.extractall(LOCAL_DIR)
         tar.close()
+#%%
 
-def read(split):
-    """Create an instance of the dataset object."""
+def load(split):
+    """Create an instance of the dataset object and preprocess images"""
     """An iterator that reads and returns images and labels from cifar."""
     batches = {
         tf.estimator.ModeKeys.TRAIN: TRAIN_BATCHES,
@@ -51,29 +55,72 @@ def read(split):
 
     all_images = []
     all_labels = []
-
+    
     for batch in batches:
         with open("%s%s%s" % (LOCAL_DIR, DATA_DIR, batch), "rb") as fo:
-            dict = cPickle.load(fo)
-            images = np.array(dict["data"])
-            labels = np.array(dict["labels"])
+            diction = pickle.load(fo, encoding='bytes')
+            images = np.array(diction[list(diction)[2]])
+            labels = np.array(diction[list(diction)[1]])
 
             num = images.shape[0]
             images = np.reshape(images, [num, 3, IMAGE_SIZE, IMAGE_SIZE])
             images = np.transpose(images, [0, 2, 3, 1])
+
+            f = plt.figure()
+            plt.imshow(images[0])
+            plt.show()
+
+
             print("Loaded %d examples." % num)
+            for image in images:
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+                mean_y = np.mean(image[:,:,0])
+                std_y = np.std(image[:,:,0])
+                image[:,:,0] = (image[:,:,0]-mean_y)/std_y
+
+            plt.imshow(images[0])
+            plt.show()
+
+
+            mean_u = np.mean(images[:,:,:,1])
+            std_u = np.std(images[:,:,:,1])
+            mean_v = np.mean(images[:,:,:,2])
+            std_v = np.std(images[:,:,:,2])
+            images[:,:,:,1] = (images[:,:,:,1]-mean_u)/std_u
+            images[:,:,:,2] = (images[:,:,:,2]-mean_v)/std_v
+
+            plt.imshow(images[0])
+            plt.show()
 
             all_images.append(images)
             all_labels.append(labels)
-
+    
+    # ! you are going to normalize y locally and normalize u and v globally
     all_images = np.concatenate(all_images)
     all_labels = np.concatenate(all_labels)
 
-    return tf.contrib.data.Dataset.from_tensor_slices((all_images, all_labels))
+    return tf.data.Dataset.from_tensor_slices((all_images, all_labels))
+#%%
+def gaussian1D(size=1, mean=0.25, std=0.5):
+    """return a 1D gaussian kernel
 
-def parse(image, label):
-    """Parse input record to features and labels."""
-    image = tf.to_float(image) / 255.0
-    image = tf.reshape(image, [IMAGE_SIZE, IMAGE_SIZE, 3])
-    return {"image": image}, {"label": label}
+    Keyword Arguments:
+        size {int} -- size of the kernel (default: {1})
+        mean {int} -- the kernel average (default: {0})
+        std {int} -- the kernel  (default: {1})
 
+    Returns:
+        [type] -- [description]
+    """
+    assert (size % 2), "make sure that size is an odd number"
+    assert (size > 0), "make sure that the size is strict positive"
+    kernel = np.array([ i - size//2 for i in range(size)])
+    kernel = (1/(np.sqrt(2*np.pi)*std))*np.exp((-1/2)*((kernel - mean)/std)**2)
+    return kernel
+
+
+
+# %%
+
+
+# %%
